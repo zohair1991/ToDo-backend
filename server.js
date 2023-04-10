1ff6e51a8b1e075b16625070e0206bdd6f4f83d9
Syed Zohair Ahmed <44361819+zohair1991@users.noreply.github.com>
2023-04-11 00:35:29 +0200

    MongoDB Atlast

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+mongoose.connect("mongodb+srv://zohair1991:Ui23cQCqCClyhsjV@cluster0.l1vsguj.mongodb.net/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
